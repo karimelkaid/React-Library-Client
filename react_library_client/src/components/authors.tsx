212491408ce6b7a1999ac8ba28b474d125e797cd
kaRim6123 <elkarim741@gmail.com>
2024-04-14 14:44:06 +0200

    The authors are loaded but not yet displayed.

--- react_library_client/src/components/authors.tsx
+++ react_library_client/src/components/authors.tsx
@@ -1,4 +1,30 @@
+import {useEffect, useState} from "react";
+import {get_authors} from "../api.ts";
+import {Author} from "../types.ts";
+
 function Authors(){
+    const [authors, setAuthors] = useState<Author[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+
+
+    useEffect(() => {
+        loadAuthors();
+    }, []);
+
+
+    const loadAuthors = async () => {
+        setLoading(true);
+
+        const authors = await get_authors();
+        console.log(authors);
+        setAuthors(authors);
+
+        setLoading(false);
+    };
+
+
+
+
     return (
         <div>
             <h1>Page des auteurs</h1>
