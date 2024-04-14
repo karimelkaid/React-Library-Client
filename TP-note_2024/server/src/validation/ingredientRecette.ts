e15924ec3dfe918327a663a23c453f094086685b
kaRim6123 <elkarim741@gmail.com>
2024-04-14 12:36:09 +0200

    Deletion of directories not required for the project.

--- TP-note_2024/server/src/validation/ingredientRecette.ts
+++
@@ -1,9 +0,0 @@
-import { object, optional, integer, min } from 'superstruct';
-
-export const IngredientRecetteCreationData = object({
-  quantite: min(integer(), 1),
-});
-
-export const IngredientRecetteUpdateData = object({
-  quantite: optional(min(integer(), 1)),
-});
