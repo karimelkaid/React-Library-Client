d0c2cd35363f9894a8e65554a40b8ebf2a5a484f
kaRim6123 <elkarim741@gmail.com>
2024-04-14 17:21:04 +0200

    It is possible to add an author from the form, adding comments to the document.

--- react_library_client/src/api.ts
+++ react_library_client/src/api.ts
@@ -1,4 +1,16 @@
+import {AuthorCreationData} from "./types.ts";
+
 const apiBasename = "http://localhost:3000";
+
+/*
+    get_authors :
+        Fetches the list of authors from the api.
+    Parameter(s) :
+        - None
+    Return :
+        - Returns an array of author objects if the request is successful.
+        - Throws an error with a relevant message if the request fails.
+*/
 
 export async function get_authors() {
     const res = await fetch(`${apiBasename}/authors`);
@@ -8,4 +20,30 @@ export async function get_authors() {
     }
     const authors = await res.json();
     return authors;
+}
+
+/*
+    add_author :
+        Sends a POST request to the api to add a new author.
+    Parameter(s) :
+        - authorCreationData: AuthorCreationData : An object containing data for creating the new author, including 'firstname' and 'lastname'.
+    Return :
+        - Returns the newly created author object if the request is successful.
+        - Throws an error with a relevant message if the request fails.
+*/
+
+export async function add_author(authorCreationData : AuthorCreationData) {
+    const res = await fetch(`${apiBasename}/authors`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(authorCreationData),
+    });
+    if (!res.ok) {
+        const msg = await res.text();
+        throw new Error(msg);
+    }
+    const author = await res.json();
+    return author;
 }
--- react_library_client/src/components/authors.tsx
+++ react_library_client/src/components/authors.tsx
@@ -1,35 +1,89 @@
-import React, { useEffect, useState } from "react";
-import { get_authors } from "../api";
-import { Author } from "../types";
+import {add_author, get_authors} from "../api";
+import {Author, AuthorCreationData} from "../types";
 import { Outlet } from "react-router-dom";
+import {useEffect, useState} from "react";
 
 function Authors() {
     const [authors, setAuthors] = useState<Author[]>([]);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        async function loadAuthors() {
-            setLoading(true);
-            try {
-                const authorsData = await get_authors();
-                console.log("Authors:", authorsData);
-                setAuthors(authorsData);
-            } catch (error) {
-                console.error("Failed to fetch authors:", error);
-            }
-            setLoading(false);
-        }
-
         loadAuthors();
     }, []);
+
+    /*
+        loadAuthors :
+            Loads the authors from the api and sets the state accordingly.
+        Parameter(s) :
+            - None
+        Return :
+            void
+     */
+    async function loadAuthors() {
+        setLoading(true);
+        try {
+            const authorsData = await get_authors();
+            console.log("Authors:", authorsData);
+            setAuthors(authorsData);
+        } catch (error) {
+            console.error("Failed to fetch authors:", error);
+        }
+        setLoading(false);
+    }
+
+    /*
+        addAuthor :
+            Adds an author to the api and reloads the list of authors.
+        Parameter(s) :
+            - authorCreationData : AuthorCreationData : The data of the author to add.
+        Return :
+            void
+    */
+    async function addAuthor(authorCreationData: AuthorCreationData) {
+        await add_author(authorCreationData)
+        loadAuthors();
+    }
+
+    /*
+        handleAdd :
+            Processes the fields entered to add an author to the api.
+        Parameter(s) :
+            - event : React.FormEvent<HTMLFormElement> : The event that triggered the function.
+        Return :
+            void
+    */
+    function handleAdd(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault(); // Don't use the default form submission behavior
+        const form = event.currentTarget;
+        const formData = new FormData(form);
+        //console.log("firstname", formData.get("firstname"));
+        const authorCreationData : AuthorCreationData = {
+            firstname: formData.get("firstname") as string,
+            lastname: formData.get("lastname") as string,
+        };
+        addAuthor(authorCreationData);
+        form.reset();
+    }
 
     if (loading) {
         return <div>Chargement...</div>;
     }
 
+
     return (
         <div id="container">
             <div id="sidebar">
+                <form onSubmit={handleAdd}>
+                    <input
+                        type="text"
+                        name="firstname"/>
+                    <input
+                        type="text"
+                        name="lastname"
+                    />
+                    <button type="submit">Add Author</button>
+                </form>
+
                 <ul>
                     {authors.map((author) => (
                         <li key={author.id}>
@@ -39,7 +93,7 @@ function Authors() {
                 </ul>
             </div>
             <div id="info">
-                <Outlet />
+                <Outlet/>
             </div>
         </div>
     );
